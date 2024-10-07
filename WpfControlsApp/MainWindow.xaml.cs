0ddb67666953faddde0f271b1e416e8d275ebcb3
Maxim Efimov <efimov.top.academy@gmail.com>
2024-10-07 19:20:49 +0300

    07-10-24

--- WpfControlsApp/MainWindow.xaml.cs
+++ WpfControlsApp/MainWindow.xaml.cs
@@ -16,9 +16,48 @@ namespace WpfControlsApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        int count = 0;
+        Brush backColor;
+
         public MainWindow()
         {
             InitializeComponent();
+            
+            Button myButton = new Button();
+            myButton.Content = "My Button";
+            myButton.FontSize = 30;
+            stack.Children.Add(myButton);
+        }
+
+        private void rptBtn_Click(object sender, RoutedEventArgs e)
+        {
+            txtTablo.Text = (++count).ToString();
+        }
+
+        private void checkCpp_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Checked!");
+        }
+
+        private void checkCpp_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checkCpp_Indeterminate(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            backColor = btn.Foreground;
+            btn.Foreground = new SolidColorBrush(Colors.Red);
+        }
+
+        private void btn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btn.Foreground = backColor;
         }
     }
 }
